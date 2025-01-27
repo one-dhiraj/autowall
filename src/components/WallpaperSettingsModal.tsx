93034053f3fde4c9b156cb5879cbdf0b437c419b
Dhiraj Kumar Sharma <dhirajksharma.social@gmail.com>
2025-01-27 10:51:13 +0530

    modified wallpaper settings modal to work with bottom navigation

--- src/components/WallpaperSettingsModal.tsx
+++ src/components/WallpaperSettingsModal.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Modal, View, Text, Pressable, StyleSheet, Animated, Switch, TouchableOpacity, Easing, ActivityIndicator, useColorScheme } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  Switch,
+  TouchableOpacity,
+  Easing,
+  ActivityIndicator,
+} from 'react-native';
 import { PropsWithChildren } from 'react';
-import { Dropdown } from 'react-native-element-dropdown';
 import { SelectList } from 'react-native-dropdown-select-list'
 import Icon from 'react-native-vector-icons/Ionicons';
-import OctIcons from 'react-native-vector-icons/Octicons'
-import {OpenOptimizationSettings } from "react-native-battery-optimization-check";
+import { useGlobalState } from './utilFunctions';
 
 type Props = PropsWithChildren<{
   isVisible: boolean;
@@ -18,26 +25,15 @@ export default function WallpaperSettings({ isVisible, onClose, setWallpaper }: 
   const [isRandom, setIsRandom] = useState<boolean>(false); // Track whether wallpapers are random or serial
   const [screen, setScreen] = useState<string>(); // Store the screen where to set wallpaper
   const [isSettingWallpaper, setIsSettingWallpaper] = useState<boolean>(false);
-  const isDarkMode = useColorScheme() === 'dark';
+  const { isDarkMode } = useGlobalState();
 
   const toggleSwitch = () => setIsRandom((prev) => !prev);
-
-  const slideAnim = useRef(new Animated.Value(300)).current; // Initially off-screen
-
-  // Animate modal in when it becomes visible
-  if (isVisible) {
-    Animated.timing(slideAnim, {
-      toValue: 0, // Fully visible (original position)
-      duration: 200,
-      easing: Easing.ease,
-      useNativeDriver: false,
-    }).start();
-  }
+  const slideAnim = useRef(new Animated.Value(400)).current; // Initially off-screen
 
   // Animate modal out when it closes
   const handleClose = () => {
     Animated.timing(slideAnim, {
-      toValue: 350, // Slide out of the view
+      toValue: 400, // Slide out of the view
       duration: 200,
       easing: Easing.ease,
       useNativeDriver: false,
@@ -69,119 +65,117 @@ export default function WallpaperSettings({ isVisible, onClose, setWallpaper }: 
 
   useEffect(()=>{
     setIsSettingWallpaper(false);
+    if (isVisible) {
+      Animated.timing(slideAnim, {
+        toValue: 0, // Fully visible (original position)
+        duration: 200,
+        easing: Easing.ease,
+        useNativeDriver: false,
+      }).start();
+    }else{
+      handleClose();
+    }
   },[isVisible])
 
+
   return (
-    <Modal animationType='fade' statusBarTranslucent transparent={true} visible={isVisible} onRequestClose={handleClose}>
-      <View style={styles.modalBackground} onTouchEnd={handleClose}>
-        <Animated.View style={[styles.modalContent, {backgroundColor: isDarkMode? "black": "white", transform: [{ translateY: slideAnim }] }]} onTouchEnd={(e)=> e.stopPropagation()}>
-          {/* Title and Close Button */}
-          <View style={[styles.container, {marginBottom: 10}]}>
-            <Text style={[styles.title, {color: isDarkMode? "white": "black"}]}>Customize Behavior</Text>
-            <Pressable onPress={handleClose}>
-              <Icon name="close" size={20} color={isDarkMode? "white": "black"} />
-            </Pressable>
-          </View>
+    <Animated.View style={[styles.modalContent, {backgroundColor: isDarkMode? "#111": "white", borderColor: isDarkMode? "grey": "lightgrey", shadowColor: isDarkMode? "white": "black", transform: [{ translateY: slideAnim }] }]} onTouchEnd={(e)=> e.stopPropagation()}>
+      {/* Title and Close Button */}
+      <View style={[styles.container, {marginBottom: 25}]}>
+        <Text style={[styles.title, {color: isDarkMode? "white": "black"}]}>Customize Wallpaper Behavior</Text>
+        {/* <Pressable onPress={handleClose}>
+          <Icon name="close" size={20} color={isDarkMode? "white": "black"} />
+        </Pressable> */}
+      </View>
 
-          <View style={styles.bodyContainer}>
-            {/* Duration Input */}
-            <View style={styles.container}>
-              <Text style={[styles.label, {color: isDarkMode? "white": "black"}]}>Duration (in hours)</Text>
-              <View style={{flexDirection: "row", flex: 2/3,}}>
-                <TouchableOpacity
-                  style={{flex: 1, backgroundColor: "#c0f1f1", borderRadius: 5, justifyContent: "center"}}
-                  onPress={()=> updateDuration(-1, duration/60)}
-                  disabled={duration==30}
-                  >
-                  <Text style={{textAlign: "center", fontSize: 16, color: `${duration>30? "black": "#fafafa"}`}}>-</Text>
-                </TouchableOpacity>
-                <Text style={{flex: 1, textAlign: "center", fontSize: 16, margin: 3, color: isDarkMode? "white": "black"}}>{duration/60}</Text>
-                <TouchableOpacity
-                  style={{flex: 1, backgroundColor: "#c0f1f1", borderRadius: 5, justifyContent: "center"}}
-                  onPress={()=> updateDuration(1, duration/60)}
-                  disabled={duration==2880}
-                  >
-                  <Text style={{textAlign: "center", fontSize: 16, color: `${duration<2880? "black": "#fafafa"}`}}>+</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-         
-            <View style={{position: "relative"}}>
-            <SelectList
-              data={[
-                {key: '2', value: 'Lock Screen'},
-                {key: '1', value: 'Home Screen'},
-                {key: '3', value: 'Both the Screens'}
-              ]}
-              setSelected={setScreen}
-              placeholder={`${screen==undefined?'Select wallpaper screen': screen}`}
-              search={false}
-              save={'value'}
-              boxStyles={{...styles.dropdown, borderColor: `${screen==undefined? "red": "grey"}`}}
-              dropdownStyles={{...styles.dropContainer, backgroundColor: isDarkMode? "#111": "white"}}
-              inputStyles={{marginLeft: 5, color: isDarkMode? "white": "black"}}
-              dropdownTextStyles={{color: isDarkMode? "white": "black"}}
-              arrowicon={<Icon name="chevron-down" size={16} color={isDarkMode? "white": "black"} style={{marginRight: 5, paddingTop: 4}}/>}
-            />
-            <Text style={{position: "absolute", bottom: -20, padding: 3, color: `${screen==undefined?"red": isDarkMode? "black": "white"}`, fontSize:12, fontWeight: 300}}>Please select a wallpaper screen</Text>
-            </View>
+      <View style={styles.bodyContainer}>
+        <View style={{position: "relative"}}>
+        <Text style={{position: "absolute", top: -20, padding: 2, color: `${screen==undefined?"red": isDarkMode? "#111": "white"}`, fontSize:12, fontWeight: 300}}>Please select a wallpaper screen</Text>
+        <SelectList
+          data={[
+            {key: '2', value: 'Lock Screen'},
+            {key: '1', value: 'Home Screen'},
+            {key: '3', value: 'Both the Screens'}
+          ]}
+          setSelected={setScreen}
+          placeholder={`${screen==undefined?'Select wallpaper screen': screen}`}
+          search={false}
+          save={'value'}
+          boxStyles={{...styles.dropdown, borderColor: `${screen==undefined? "red": "grey"}`}}
+          dropdownStyles={{...styles.dropContainer, backgroundColor: isDarkMode? "#111": "white"}}
+          inputStyles={{marginLeft: 5, color: isDarkMode? "white": "black"}}
+          dropdownTextStyles={{color: isDarkMode? "white": "black"}}
+          arrowicon={<Icon name="chevron-down" size={16} color={isDarkMode? "white": "black"} style={{marginRight: 5, paddingTop: 4}}/>}
+        />
+        </View>
 
-            {/* Random/Serial Switch */}
-            <View style={styles.container}>
-              <Text style={[styles.label, {color: isDarkMode? "white": "black"}]}>Shuffle between Wallpapers</Text>
-              <Switch
-                value={isRandom}
-                onValueChange={toggleSwitch}
-                thumbColor={`${isRandom? "lightgreen" : "#c0f1f1"}`}
-                trackColor={{ false: "#c0f1f1", true: "lightgreen" }}
-              />
-            </View>
-
-            <View style={styles.container}>
-            <Text style={{fontSize: 14, textAlign: "justify", color: isDarkMode? "#ddd": "rgb(77, 78, 79)"}}>Disable Battery Optimizations{`\n`}for reliable wallpaper transitions</Text>
-            <Pressable onPress={OpenOptimizationSettings}>
-                <OctIcons style={{marginRight: 15}} name="link-external" size={20} color={isDarkMode? "#ddd":"rgb(77, 78, 79)"}/>
-            </Pressable>
-            </View>
-
-            {/* Set Wallpaper Button */}
+        {/* Duration Input */}
+        <View style={styles.container}>
+          <Text style={[styles.label, {color: isDarkMode? "white": "black"}]}>Duration (in hours)</Text>
+          <View style={{flexDirection: "row", flex: 2/3,}}>
             <TouchableOpacity
-              activeOpacity={0.6}
-              style={[styles.button, { backgroundColor: 'lightgreen', opacity: Number(`${screen==undefined ? 0.5 : 1}`) }]}
-              onPress={handleSetWallpaper}
-              disabled={screen==undefined || isSettingWallpaper}
-            >
-              {isSettingWallpaper?
-                <>
-                <ActivityIndicator size="small" color="grey" />
-                <Text style={{color: 'grey', fontSize: 16}}>  Setting Wallpapers</Text>
-                </>
-                :
-                <Text style={{color: `${screen==undefined ? '#fff' : '#000'}`, fontSize: 16,}}>Set Wallpapers</Text>
-              }
+              style={{flex: 1, backgroundColor: "#c0f1f1", borderRadius: 5, justifyContent: "center", opacity: duration>30? 1: 0.4}}
+              onPress={()=> updateDuration(-1, duration/60)}
+              disabled={duration==30}
+              >
+              <Text style={{textAlign: "center", fontSize: 16, color: `${duration>30? "black": isDarkMode? "black": "grey"}`}}>-</Text>
+            </TouchableOpacity>
+            <Text style={{flex: 1, textAlign: "center", fontSize: 16, margin: 3, color: isDarkMode? "white": "black"}}>{duration/60}</Text>
+            <TouchableOpacity
+              style={{flex: 1, backgroundColor: "#c0f1f1", borderRadius: 5, justifyContent: "center", opacity: duration<2880? 1: 0.4}}
+              onPress={()=> updateDuration(1, duration/60)}
+              disabled={duration==2880}
+              >
+              <Text style={{textAlign: "center", fontSize: 16, color: `${duration<2880? "black": isDarkMode? "black": "grey"}`}}>+</Text>
             </TouchableOpacity>
           </View>
-        </Animated.View>
+        </View>
+
+
+        {/* Random/Serial Switch */}
+        <View style={styles.container}>
+          <Text style={[styles.label, {color: isDarkMode? "white": "black"}]}>Shuffle between Wallpapers</Text>
+          <Switch
+            value={isRandom}
+            onValueChange={toggleSwitch}
+            thumbColor={`${isRandom? "lightgreen" : "#c0f1f1"}`}
+            trackColor={{ false: "#c0f1f1", true: "lightgreen" }}
+          />
+        </View>
+
+        {/* Set Wallpaper Button */}
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor: 'lightgreen', opacity: Number(`${screen==undefined ? 0.5 : 1}`) }]}
+          onPress={handleSetWallpaper}
+          disabled={screen==undefined || isSettingWallpaper}
+        >
+          {isSettingWallpaper?
+            <>
+            <ActivityIndicator size="small" color="grey" />
+            <Text style={{color: 'grey', fontSize: 16}}>  Setting Wallpapers</Text>
+            </>
+            :
+            <Text style={{color: `${screen==undefined ? '#fff' : '#000'}`, fontSize: 16,}}>Set Wallpapers</Text>
+          }
+        </TouchableOpacity>
       </View>
-    </Modal>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
-  modalBackground: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    backgroundColor: "rgba(60, 60, 60, 0.7)",
-  },
   modalContent: {
-    height: 350,
-    width: "100%",
+    height: 275,
+    width: "95%",
     maxWidth: 500,
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
+    borderRadius: 20,
     padding: 15,
+    bottom: 60,
+    left: "2.5%",
+    position: "absolute",
+    borderWidth: 0.5,
+    elevation: 6
   },
   container: {
     flexDirection: 'row',
@@ -195,10 +189,10 @@ const styles = StyleSheet.create({
   bodyContainer: {
     flex: 1,
     justifyContent: "space-between",
-    paddingTop: 5
   },
   label: {
     fontSize: 14,
+    marginLeft: 5
   },
   dropdown: {
     borderWidth: 0.5,
@@ -220,6 +214,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
-    marginVertical: 3,
+    
   },
 });
